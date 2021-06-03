34e7a7f1583e19ab8f2e1a68ca8b0d71610c2344
Qifeng Sun <troy@MacBook-Pro.local>
2021-06-02 20:09:02 -0400

    first commit

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Notfound = () => {
+  return (
+    <div>
+      <h2>404</h2>
+      <p>Sorry, that page doesn't exist</p>
+    </div>
+  )
+}
+
+export default Notfound
